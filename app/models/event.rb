4c296efa94731b99c86a1742b8da541360ef6469
annmagdalen <annmagdalen@hotmail.com>
2017-05-23 11:38:55 +0200

    One-to-Many: Controllers

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,12 +3,12 @@ class Event < ApplicationRecord
 
   validates :name, :start_at, presence:true
   validates :description, presence:true, length:{maximum:500}
-  validates :end_at, presence:true
+  validates :ends_at, presence:true
   validate :end_after_start?
 
   def end_after_start?
-    if end_at < start_at
-      errors.add :end_at, "Must be after start date"
+    if ends_at < start_at
+      errors.add(:ends_at, "Must be after start date")
     end
   end
 end
